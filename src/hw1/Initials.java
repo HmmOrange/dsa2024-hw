95a0891ab6640c23f3bf4685486e27b6f14a1ba6
HmmOrange <leanhduy0206@gmail.com>
2024-09-15 21:30:03 +0700

    First commit

---
+++ src/hw1/Initials.java
@@ -0,0 +1,17 @@
+package hw1;
+
+public class Initials {
+    public static void main(String[] args){
+        System.out.println("""
+        **********      **           **  **            **
+        **        **    **           **   **          **
+        **         **   **           **    **        **
+        **          **  **           **     **      **
+        **          **  **           **       *******
+        **          **  **           **         **
+        **         **   **           **         **
+        **        **     **         **          **
+        **********         *********            **
+        """);
+    }
+}
